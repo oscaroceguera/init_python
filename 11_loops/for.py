eda5ca0b078b90a4b6a6ad0580bf70a200a0ad4c
oscar <oscaroceguera@gmail.com>
2020-03-31 22:20:55 -0700

    Booleans, If statements, The in keyword, Loops

---
+++ 11_loops/for.py
@@ -0,0 +1,7 @@
+friends = ["Rolf", "Jen", "Bob", "Anne", "CArlos", "Emilio", "FRacnisco"]
+
+for friend in friends:
+    print(f"{friend} is my friend")
+
+for friend in range(4):
+    print(f"{friend} is my friend")
