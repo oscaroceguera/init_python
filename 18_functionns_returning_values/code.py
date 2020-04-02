468d4e47595e320854b2e9a008050ebd7809aa28
oscar <oscaroceguera@gmail.com>
2020-04-02 15:58:02 -0700

    functions returning value

---
+++ 18_functionns_returning_values/code.py
@@ -0,0 +1,18 @@
+def add(x, y):
+    return x + y
+
+
+add(5, 8)
+result = add(5, 8)
+print(result)
+
+
+def divide(divided, divisor):
+    if divisor != 0:
+        return divided / divisor
+    else:
+        return "You fool!"
+
+
+result = divide(15, 2) * 5
+print(result)
